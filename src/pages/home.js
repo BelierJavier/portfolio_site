897601d1350bbe71bb7c2cd92f26eb4ed2d41a2f
BelierJavier <Zathoroth@icloud.com>
2022-11-26 02:17:57 -0500

    Home Page Update
    + Header
    + Caption

--- src/pages/home.js
+++ src/pages/home.js
@@ -15,19 +15,32 @@ return (
         width={1/2}
         color='white'
         bg='primary'>
+
 	        <div className="page">
                 <div className="page-header">
 	                <h1>
-		                Prodigy Alpha's home page.
+		                Hey 👋🏽 , I'm Belly.
 	                </h1>
+                    <p className="text">
+                        I'm a software developer who enjoys building websites and mobile 
+                        apps for independent clients, small, and medium sized businesses. 
+                        If you're an individual or growing company trying to establish 
+                        your brand on the internet, from online blogs and portfolios to 
+                        virtual stores, It'll be my pleasure to bring your services to 
+                        the next level.
+                    </p>
                 </div>
 	        </div>
+
         </Box>
         <Box
         p={3}
         width={1/2}
         color='white'
         bg='primary'>
+
+
+
         </Box>
         <Box
         p={3}
